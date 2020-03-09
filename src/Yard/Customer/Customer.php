160cacb65ac5d4be8012c8ec92142900ed7f9a85
zhangxiang <zhangxiang_php@vchangyi.com>
2020-03-09 11:40:49 +0800

    refactor:composer

---
+++ src/Yard/Customer/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: <zhangxiang_php@vchangyi.com>
+ * Date: 2020/3/7 Time: 下午5:13
+ */
+
+namespace ThingYard\Yard\Customer;
+
+
+use ThingYard\Yard\BaseClient;
+
+class Customer extends BaseClient
+{
+
+}
