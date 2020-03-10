ee15babf8da25a9b1ef380a59c9ab09db53af831
zhangxiang <zhangxiang_php@vchangyi.com>
2020-03-10 11:57:52 +0800

    feat:增加url拼接辅助方法

--- src/Yard/BaseClient.php
+++ src/Yard/BaseClient.php
@@ -2,6 +2,7 @@
 
 namespace ThingYard\Yard;
 
+use ThingYard\Kernel\Exceptions\InvalidArgumentException;
 use ThingYard\Kernel\KernelClient;
 use ThingYard\Kernel\Middleware\YardRequestIdMiddleware;
 
@@ -31,7 +32,47 @@ abstract class BaseClient extends KernelClient
     {
         return ltrim(sprintf(
             $url,
-            $this->app->config['qa.env']
+            $this->app->config['env']
         ),'/');
+    }
+
+    /**
+     * @param $url
+     * User: <zhangxiang_php@vchangyi.com>
+     * @return string
+     * Date: 2020/3/9 Time: 下午2:23
+     * @throws InvalidArgumentException
+     */
+    protected function urlCompanyId($url)
+    {
+        if (empty($this->app->config['companyId'])) {
+            throw new InvalidArgumentException('company_id is invalid');
+        }
+        $query = [
+            'companyId' => $this->app->config['qa.companyId']
+        ];
+        $queryString = http_build_query($query);
+        $url .= '?'.$queryString;
+
+        return $this->realUrl($url);
+    }
+
+    /**
+     * 查询字符串
+     *
+     * @param $url
+     * User: <zhangxiang_php@vchangyi.com>
+     * @param array $query
+     * @return string
+     * Date: 2020/3/9 Time: 下午2:23
+     */
+    protected function queryString($url, array $query)
+    {
+        if (!empty($query)) {
+            $queryString = http_build_query($query);
+            $url .= '?'.$queryString;
+        }
+
+        return $this->realUrl($url);
     }
 }
