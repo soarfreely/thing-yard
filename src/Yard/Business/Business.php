f1c0a921c516c8b8a134ce85f4f3a3bd80764eb1
zhangxiang <zhangxiang_php@vchangyi.com>
2020-03-10 11:56:43 +0800

    feat:B端接口封装

--- src/Yard/Business/Business.php
+++ src/Yard/Business/Business.php
@@ -11,14 +11,14 @@ use GuzzleHttp\Exception\GuzzleException;
 use Psr\Http\Message\ResponseInterface;
 use ThingYard\Kernel\Exceptions\AuthorizationException;
 use ThingYard\Kernel\Exceptions\BadRequestException;
+use ThingYard\Kernel\Exceptions\InvalidArgumentException;
 use ThingYard\Kernel\Exceptions\InvalidConfigException;
 use ThingYard\Kernel\Exceptions\ResourceNotFoundException;
 use ThingYard\Kernel\Exceptions\ServiceInvalidException;
 use ThingYard\Kernel\Exceptions\ValidationException;
 use ThingYard\Kernel\Support\Collection;
-use ThingYard\Yard\BaseClient;
 
-class Business extends BaseClient
+class Business extends BusinessClient
 {
 
     /**
@@ -34,10 +34,11 @@ class Business extends BaseClient
      * @throws ResourceNotFoundException
      * @throws ServiceInvalidException
      * @throws ValidationException Date: 2020/3/8 Time: 下午3:06
+     * @throws InvalidArgumentException
      */
     public function login(array $params)
     {
-        $url = $this->realUrl('%s/data/isv/login/v1');
+        $url = $this->urlCompanyId('%s/data/isv/login/v1');
         return $this->httpPostJson($url, $params);
     }
 
@@ -54,11 +55,142 @@ class Business extends BaseClient
      * @throws ResourceNotFoundException
      * @throws ServiceInvalidException
      * @throws ValidationException
+     * @throws InvalidArgumentException
      * Date: 2020/3/8 Time: 下午10:25
      */
-    public function store(array $params)
+    public function stores(array $params)
     {
-        $url = $this->realUrl('%s/%s/tm/isv/stores/v1');
+        $url = $this->urlCompanyId('%s/tm/isv/stores/v1');
         return $this->httpPostJson($url, $params);
+    }
+
+    /**
+     * 同步用户（批量同步）
+     *
+     * @param array $params
+     * User: <zhangxiang_php@vchangyi.com>
+     * @return array|object|ResponseInterface|string|Collection
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     * @throws InvalidConfigException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException
+     * Date: 2020/3/9 Time: 下午2:35
+     */
+    public function sync(array $params)
+    {
+        $url = $this->urlCompanyId('%s/tm/isv/users/v1');
+        return $this->httpPostJson($url, $params);
+    }
+
+    /**
+     * 防伪扫码
+     *
+     * @param array $params
+     * User: <zhangxiang_php@vchangyi.com>
+     * @return array|object|ResponseInterface|string|Collection
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws GuzzleException
+     * @throws InvalidConfigException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException
+     * Date: 2020/3/9 Time: 下午2:38
+     */
+    public function innerCode(array $params)
+    {
+        $url = $this->realUrl('%s/tm/isv/tag/v1/innercode');
+        return $this->httpPostJson($url, $params);
+    }
+
+    /**
+     * 扫明码或箱码入库
+     *
+     * @param array $params
+     * User: <zhangxiang_php@vchangyi.com>
+     * @return array|object|ResponseInterface|string|Collection
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws GuzzleException
+     * @throws InvalidConfigException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException
+     * Date: 2020/3/9 Time: 下午2:39
+     */
+    public function outerCode(array $params)
+    {
+        $url = $this->realUrl('%s/tm/isv/tag/v1/outercode');
+        return $this->httpPostJson($url, $params);
+    }
+
+    /**
+     * 经销商添加（批量添加）
+     *
+     * @param array $params
+     * User: <zhangxiang_php@vchangyi.com>
+     * @return array|object|ResponseInterface|string|Collection
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws GuzzleException
+     * @throws InvalidConfigException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException
+     * @throws InvalidArgumentException
+     * Date: 2020/3/9 Time: 下午2:42
+     */
+    public function dealer(array $params)
+    {
+        $url = $this->urlCompanyId('%s/data/isv/dealers/v1');
+        return $this->httpPostJson($url, $params);
+    }
+
+
+    /**
+     * 经销商修改（批量修改）
+     *
+     * User: <zhangxiang_php@vchangyi.com>
+     * @param array $params
+     * @return array|object|ResponseInterface|string|Collection
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     * @throws InvalidConfigException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException Date: 2020/3/9 Time: 下午2:56
+     */
+    public function modifyDealer(array $params)
+    {
+       $url =  $url = $this->urlCompanyId('%s/data/isv/dealers/v1');
+       return $this->httpPutJson($url, $params);
+    }
+
+    /**
+     * 经销商删除（批量删除）
+     *
+     * @param array $params
+     * User: <zhangxiang_php@vchangyi.com>
+     * @return array|object|ResponseInterface|string|Collection|null
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException Date: 2020/3/9 Time: 下午3:46
+     * @throws InvalidConfigException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException
+     */
+    public function delDealer(array $params)
+    {
+        $url =  $url = $this->urlCompanyId('%s/data/isv/dealers/v1');
+//        $url = 'http://local.ka:8082/admin/supplierAuth/delete';
+        return $this->httpDelJson($url, $params);
     }
 }
---
+++ src/Yard/Business/BusinessClient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: <zhangxiang_php@vchangyi.com>
+ * Date: 2020/3/9 Time: 下午2:10
+ */
+
+namespace ThingYard\Yard\Business;
+
+
+use ThingYard\Yard\BaseClient;
+
+abstract class BusinessClient extends BaseClient
+{
+
+}
