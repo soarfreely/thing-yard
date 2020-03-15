11cc34225cbc2d871c5e9893a37c9e929525398f
zhangxiang <zhangxiang_php@vchangyi.com>
2020-03-15 14:30:26 +0800

    refactor:BaseClient

--- src/Yard/BaseClient.php
+++ src/Yard/BaseClient.php
@@ -4,10 +4,35 @@ namespace ThingYard\Yard;
 
 use ThingYard\Kernel\Exceptions\InvalidArgumentException;
 use ThingYard\Kernel\KernelClient;
+use ThingYard\Kernel\Middleware\BearerTokenMiddleware;
 use ThingYard\Kernel\Middleware\YardRequestIdMiddleware;
 
 abstract class BaseClient extends KernelClient
 {
+    /**
+     * 请求成功状态吗
+     */
+    const HTTP_STATUS_SUCCESS = [200];
+
+    /**
+     * 网关
+     *
+     * @var string
+     */
+    protected $gateway = 'api-gateway';
+
+    /**
+     * 企业id
+     * @var string
+     */
+    protected $companyId = '';
+
+    /**
+     * 企业id
+     * @var array
+     */
+    protected $company = [];
+
     /**
      * 中间件注册
      */
@@ -18,7 +43,45 @@ abstract class BaseClient extends KernelClient
             YardRequestIdMiddleware::getAccessName()
         );
 
-//        parent::registerMiddleware();
+        $this->pushMiddleware(
+            $this->app['token_middleware'],
+            BearerTokenMiddleware::getAccessName()
+        );
+
+        parent::registerMiddleware();
+    }
+
+    /**
+     *  设置 basic_uri
+     * @param $baseUri
+     * @return $this
+     * @throws InvalidArgumentException Date: 2020/3/12 Time: 下午11:33
+     */
+    protected function setBaseUri($baseUri)
+    {
+        if (empty($baseUri)) {
+            throw new InvalidArgumentException('host is expected');
+        }
+
+        $this->baseUri = $baseUri;
+        return $this;
+    }
+
+    /**
+     * 设置CompanyId
+     *
+     * Date: 2020/3/9 Time: 下午2:23
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    protected function setCompanyId()
+    {
+        if (empty($companyId = $this->app->config->get('company_id', ''))) {
+            throw new InvalidArgumentException('company id is expected');
+        }
+
+        $this->companyId = $companyId;
+        return $this;
     }
 
     /**
@@ -26,53 +89,25 @@ abstract class BaseClient extends KernelClient
      *
      * @param string $url
      *
+     * @param array $query
      * @return string
      */
-    protected function realUrl($url)
+    protected function realUrl($url, $query = [])
     {
-        return ltrim(sprintf(
-            $url,
-            $this->app->config['env']
-        ),'/');
+        return $this->queryString($url, $this->gateway, $query);
     }
 
-    /**
-     * @param $url
-     * User: <zhangxiang_php@vchangyi.com>
-     * @return string
-     * Date: 2020/3/9 Time: 下午2:23
-     * @throws InvalidArgumentException
-     */
-    protected function urlCompanyId($url)
-    {
-        if (empty($this->app->config['companyId'])) {
-            throw new InvalidArgumentException('company_id is invalid');
-        }
-        $query = [
-            'companyId' => $this->app->config['qa.companyId']
-        ];
-        $queryString = http_build_query($query);
-        $url .= '?'.$queryString;
-
-        return $this->realUrl($url);
-    }
 
     /**
      * 查询字符串
      *
      * @param $url
-     * User: <zhangxiang_php@vchangyi.com>
+     * @param string $gateway
      * @param array $query
-     * @return string
-     * Date: 2020/3/9 Time: 下午2:23
+     * @return mixed
      */
-    protected function queryString($url, array $query)
+    protected function queryString($url, $gateway = '', array $query = [])
     {
-        if (!empty($query)) {
-            $queryString = http_build_query($query);
-            $url .= '?'.$queryString;
-        }
-
-        return $this->realUrl($url);
+        return vsprintf($url, [$gateway, empty($queryString = http_build_query($query)) ? '' : "?$queryString"]);
     }
 }
